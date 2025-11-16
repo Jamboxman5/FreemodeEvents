06a69e545121c4fc391a2637e1968221e3d8f45c
Samuel Thompson <samthompson1818@gmail.com>
2025-11-16 02:28:44 -0500

    Added /events start for people to manually start work events

--- src/main/java/net/jahcraft/freemodeevents/commands/EventsCommand.java
+++ src/main/java/net/jahcraft/freemodeevents/commands/EventsCommand.java
@@ -6,11 +6,16 @@ import net.jahcraft.freemodeevents.events.challenges.RampageEvent;
 import net.jahcraft.freemodeevents.events.vip.ExecutiveSearchEvent;
 import net.jahcraft.freemodeevents.events.chat.UnscrambleEvent;
 import net.jahcraft.freemodeevents.main.Main;
-import org.bukkit.ChatColor;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.*;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.chat.hover.content.Text;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.awt.*;
 
 public class EventsCommand implements CommandExecutor {
     @Override
@@ -20,6 +25,9 @@ public class EventsCommand implements CommandExecutor {
         if (args.length == 0) {
             sender.sendMessage(ChatColor.RED + "Available subcommands: ");
             sender.sendMessage(ChatColor.RED + "/events next");
+            if (sender.hasPermission("freemodeevents.start")) {
+                sender.sendMessage(ChatColor.RED + "/events start");
+            }
             if (sender.hasPermission("freemodeevents.admin")) {
                 sender.sendMessage(ChatColor.RED + "/events trigger");
                 sender.sendMessage(ChatColor.RED + "/events cancel");
@@ -49,6 +57,32 @@ public class EventsCommand implements CommandExecutor {
             if (args[1].equalsIgnoreCase("unscramble")) Main.plugin.runEvent(new UnscrambleEvent());
             if (args[1].equalsIgnoreCase("executivesearch") && sender instanceof Player p) Main.plugin.runEvent(new ExecutiveSearchEvent(p, p.getLocation()));
             sender.sendMessage("New event triggered.");
+        }
+        else if (args[0].equalsIgnoreCase("start")) {
+            if (!sender.hasPermission("freemodeevents.start")) {
+                sender.sendMessage(ChatColor.RED + "You don't have permission to do that!");
+                return true;
+            }
+            if (!(sender instanceof Player p)) return true;
+            if (Main.plugin.isRunningEvent()) {
+                sender.sendMessage(ChatColor.RED + "You can't start a new event while one is already running!");
+                return true;
+            }
+            if (!Main.plugin.canRunEvent(true)) return true;
+            if (args.length == 1) {
+
+                TextComponent msg = new TextComponent(TextComponent.fromLegacy("§x§F§F§D§7§0§0Executive Search"));
+                msg.setUnderlined(true);
+                msg.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/events start executivesearch"));
+                msg.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("§x§0§0§E§8§F§FClick to start event!")
+                ));
+
+                sender.sendMessage(ChatColor.of("#49B3FF") + "Select one of the following available events: (Click to start)");
+                sender.spigot().sendMessage(msg);
+                return true;
+            }
+            if (args[1].equalsIgnoreCase("executivesearch")) Main.plugin.runEvent(new ExecutiveSearchEvent(p, p.getLocation()));
+            return true;
         }
         else if (args[0].equalsIgnoreCase("cancel")) {
             if (!sender.hasPermission("freemodeevents.admin")) {
