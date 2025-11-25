20db3ca4f35e8512ecf48af35b85ea0058b6ceb1
Samuel Thompson <samthompson1818@gmail.com>
2025-11-25 02:18:51 -0500

    Added trivia settings & default questions to config.yml

---
+++ src/main/java/net/jahcraft/freemodeevents/events/chat/TriviaEvent.java
@@ -0,0 +1,108 @@
+package net.jahcraft.freemodeevents.events.chat;
+
+import net.jahcraft.freemodeevents.events.FreemodeEvent;
+import net.jahcraft.freemodeevents.main.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TriviaEvent extends FreemodeEvent {
+
+    private final String phrase;
+    private final int timeLimit;
+    private final boolean easyMode;
+
+    private Player winner = null;
+
+    public TriviaEvent() {
+        List<String> phrases = Main.config.getConfig().getStringList("unscramble-phrases");
+        this.phrase = phrases.get((int) (Math.random() * phrases.size()));
+        this.timeLimit = Main.config.getConfig().getInt("unscramble-timer");
+
+        int selectedDifficulty = Main.config.getConfig().getInt("unscramble-difficulty");
+        if (selectedDifficulty == 0) easyMode = true;
+        else if (selectedDifficulty == 1) easyMode = false;
+        else {
+            double hardChance = Main.config.getConfig().getDouble("unscramble-difficulty-chance");
+            easyMode = (Math.random() > hardChance);
+        }
+    }
+
+    public TriviaEvent(String phrase, int timeLimit, boolean easyMode) {
+        this.phrase = phrase;
+        this.timeLimit = timeLimit;
+        this.easyMode = easyMode;
+
+        Main.plugin.getServer().getPluginManager().registerEvents(this, Main.plugin);
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onChat(AsyncPlayerChatEvent event) {
+        if (!Main.plugin.isRunningEvent(this)) return;
+        if (event.getMessage().equalsIgnoreCase(phrase)) event.setCancelled(true);
+
+        winner = event.getPlayer();
+
+        finish();
+
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            Bukkit.broadcastMessage("Unscramble the following phrase to win a prize: " + scramble(phrase));
+            Thread.sleep(1000L * timeLimit);
+            if (Main.plugin.isRunningEvent(this)) {
+                finish();
+            }
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private String scramble(String word) {
+        ArrayList<Character> phraseList = new ArrayList<>();
+        for (char c : word.toCharArray()) phraseList.add(c);
+
+        ArrayList<Character> scrambledList;
+        if (!easyMode) {
+            Collections.shuffle(phraseList);
+            scrambledList = phraseList;
+        } else {
+            scrambledList = new ArrayList<>();
+            scrambledList.add(phraseList.get(0));
+            scrambledList.add(phraseList.get(phraseList.size() - 1));
+            phraseList.remove(0);
+            phraseList.remove(phraseList.size()-1);
+            Collections.shuffle(phraseList);
+            for (char c : phraseList) scrambledList.add(1, c);
+
+        }
+
+        StringBuilder scrambled = new StringBuilder();
+        for (char c : scrambledList) scrambled.append(c);
+
+        return scrambled.toString().toUpperCase();
+    }
+
+    @Override
+    public void finish() {
+
+        Main.plugin.finishEvent(this);
+
+        if (winner == null) {
+            Bukkit.broadcastMessage("Nobody unscrambled the phrase in time! (" + phrase + ") Try again next time! ");
+        } else {
+            Bukkit.broadcastMessage(winner.getDisplayName() + " has unscrambled the phrase! (" + phrase + ")");
+        }
+    }
+}
